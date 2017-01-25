51ef9e1bb3bbe7fa93ec75bc2184a1a79ffa8149
Ivan Kasatenko <sky.31338@gmail.com>
2017-01-25 16:36:26 +0600

    Rails SLIM capturing support, addresses issue #133

--- spec/deface/slim_converter_spec.rb
+++ spec/deface/slim_converter_spec.rb
@@ -26,6 +26,13 @@ module Deface
             li = link_to 'Log out', logout_path
 })).to eq("<nav id=\"top-nav-bar\"><ul class=\"inline\" data-hook=\"\" id=\"nav-bar\"><% if true %><div class=\"nav-links high_res\"><li class=\"dropdown\"><div class=\"welcome\">Welcome <%= ::Temple::Utils.escape_html_safe((Spree::User.first.email)) %> &#9662;</div><ul class=\"dropdown\"><li><%= ::Temple::Utils.escape_html_safe((link_to 'Account', account_path)) %></li><li><%= ::Temple::Utils.escape_html_safe((link_to 'Log out', logout_path)) %></li></ul></li></div><% end %></ul></nav>")
       end
+
+      it "should handle Rails capturing" do
+        expect(slim_to_erb(%q{#wishlist-form
+	= form_for Spree::WishedProduct.new do |f|
+		= f.submit 'Save'
+})).to eq("<div id=\"wishlist-form\"><% _slim_controls1 = form_for Spree::WishedProduct.new do |f| %><%= ::Temple::Utils.escape_html_safe((f.submit 'Save')) %><% end %><%= ::Temple::Utils.escape_html_safe((_slim_controls1)) %></div>")
+      end
     end
 
   end
